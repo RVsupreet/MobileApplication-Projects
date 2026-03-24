90f78aedd8082bee6ec12ab74334ffa76ab497ef
RVsupreet <231701057@rajalakshmi.edu.in>
2026-03-24 13:11:38 +0530

    completed 1a

--- Excercise 1- A/GuiComponents/app/src/main/java/com/example/guicomponents/MainActivity.kt
+++ Excercise 1- A/GuiComponents/app/src/main/java/com/example/guicomponents/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.example.guicomponents
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private lateinit var btnSize: Button
+    private lateinit var btnTextColor: Button
+    private lateinit var btnBgColor: Button
+
+    private var textSize = 10f
+
+    private val colors = arrayOf(Color.RED, Color.BLUE, Color.GREEN)
+    private var textColorIndex = 0
+    private var bgColorIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        textView = findViewById(R.id.textView)
+        btnSize = findViewById(R.id.btnSize)
+        btnTextColor = findViewById(R.id.btnTextColor)
+        btnBgColor = findViewById(R.id.btnBgColor)
+
+        // Change Text Size
+        btnSize.setOnClickListener {
+            textSize += 5f
+            if (textSize > 25f) {
+                textSize = 5f
+            }
+            textView.textSize = textSize
+        }
+
+        // Change Text Color
+        btnTextColor.setOnClickListener {
+            textColorIndex = (textColorIndex + 1) % colors.size
+            textView.setTextColor(colors[textColorIndex])
+        }
+
+        // Change Background Color
+        btnBgColor.setOnClickListener {
+            bgColorIndex = (bgColorIndex + 1) % colors.size
+            textView.setBackgroundColor(colors[bgColorIndex])
         }
     }
 }
